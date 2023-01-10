38d451ce393e0e5bd4828423de95110c3a19d196
郭哲文 <kuochewen@Wens-Mac.local>
2023-01-10 15:25:36 +0800

    Add: Files

--- src/main.c
+++ src/main.c
@@ -10,18 +10,18 @@ int main(){
     for(int i = 1; i <= ITERATION; i++){
         strcpy(str, "\0");
         printf("Arithmetic Approach FILE-%d\n\n", i);
-        sprintf(str, "./obj/arth.o -e ./GrepTextFile/File%d.txt ./io_files/arith/arith_out", i);
+        sprintf(str, "./obj/arth.o -e ./GrepTextFile/File%d.txt ./IO/arith/arith_out", i);
         system(str);
-        system("./obj/arth.o -d ./io_files/arith/arith_out ./io_files/arith/arith_back.txt");
+        system("./obj/arth.o -d ./IO/arith/arith_out ./IO/arith/arith_back.txt");
         puts("------------------------------");
     }
     puts("\n-----------Huffmann Encoding-----------");
     for(int i = 1; i <= ITERATION; i++){
         printf("Huffmann Approach FILE-%d\n\n", i);
         strcpy(str, "\0");
-        sprintf(str, "./obj/huff.o -e ./GrepTextFile/File%d.txt -b ./io_files/huff/huff_booktable.txt -o ./io_files/huff/huff_out.txt", i);
+        sprintf(str, "./obj/huff.o -e ./GrepTextFile/File%d.txt -b ./IO/huff/huff_booktable.txt -o ./IO/huff/huff_out.txt", i);
         system(str);
-        system("./obj/huff.o -d ./io_files/huff/huff_out -b ./io_files/huff/huff_booktable.txt -o io_files/huff/huff_back.txt");
+        system("./obj/huff.o -d ./IO/huff/huff_out -b ./IO/huff/huff_booktable.txt -o IO/huff/huff_back.txt");
         puts("------------------------------");
     }
 
